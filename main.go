a104f5237bf1436aaf20378ee653ae0f5e21d3d7
Mike Heft <mikeheft@gmail.com>
2024-08-04 12:18:11 -0600

    initial

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("[ERROR] ", err)
+		os.Exit(1)
+	}
+
+}
