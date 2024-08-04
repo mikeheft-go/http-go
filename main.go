af7abc594ecd0e130696b9ba23060099abf99cef
Mike Heft <mikeheft@gmail.com>
2024-08-04 13:17:17 -0600

    Add Write interface

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
@@ -15,5 +17,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(b []byte) (int, error) {
+	fmt.Println(string(b))
+	return len(b), nil
 }
